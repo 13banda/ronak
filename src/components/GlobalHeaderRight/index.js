2c75191056840d1a8147c1754ac2a0570d5518b3
13banda <wwaheguru9509088985@gmail.com>
2020-04-03 10:03:04 +0530

    add user avatar

--- src/components/GlobalHeaderRight/index.js
+++ src/components/GlobalHeaderRight/index.js
@@ -1,16 +1,16 @@
 import "./index.less";
 import React from "react";
 import { useAuth0 } from "auth0";
+import AvatarDropDown from "./AvatarDropDown";
 
-function GlobalHeaderRight(props){
-    const { loading, user } = useAuth0();
-    console.log(user)
-    if(loading || !user){
-        return <div>Loading...</div>
-    }
-    return <div>
-        right content
-    </div>
+function GlobalHeaderRight(props) {
+  const { loading, user } = useAuth0();
+  console.log(user);
+  let content = <AvatarDropDown />;
+  if (loading || user) {
+    content = <div>Loading...</div>;
+  }
+  return <div className="ant-global-header-right-content">{content}</div>;
 }
 
 export default GlobalHeaderRight;
